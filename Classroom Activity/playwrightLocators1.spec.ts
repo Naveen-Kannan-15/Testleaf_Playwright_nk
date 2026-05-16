1460119fd2551a6def5cb4ea4ddf0557f523e11f
Naveen-Kannan-15 <naveenkannan1501@gmail.com>
2026-05-16 11:40:29 +0530

    Delete Classroom Activity directory

--- Classroom Activity/playwrightLocators1.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test } from "@playwright/test";
-test ('Learn Playwright Locators', async ({page}) => {
-    await page.goto("https://login.salesforce.com/?locale.in")
-    await page.getByRole("textbox", {name:"Username"}).fill("dilipkumar.rajendran@testleaf.com")
-    await page.getByRole("textbox", {name:"Password"}).fill("Testleaf@2025")
-    await page.getByRole("button", {name:"Log in"}).click()
-})
