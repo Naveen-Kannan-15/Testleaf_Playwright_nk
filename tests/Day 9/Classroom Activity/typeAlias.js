6a7637e2cfe75aa9b22a312b910d9d31e2a1de1a
Naveen-Kannan-15 <naveenkannan1501@gmail.com>
2026-05-25 17:44:24 +0530

    TestLeaf Practice Files
    TestLeaf Practice Files

---
+++ tests/Day 9/Classroom Activity/typeAlias.js
@@ -0,0 +1,9 @@
+"use strict";
+// // Type Alias for Data Types (|) with Union type
+// console.log("Type Alias for Data Types (|) with Union type")
+function makePayment(methodName) {
+    console.log("The Payment method chosen is", methodName);
+}
+makePayment("UPI");
+makePayment("creditCard");
+makePayment("scanner");
