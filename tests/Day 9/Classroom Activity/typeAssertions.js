24127561371fbcfe05b63f32cb469139727748c5
Naveen-Kannan-15 <naveenkannan1501@gmail.com>
2026-05-30 13:19:07 +0530

    TestLeaf Practice Files
    TestLeaf Practice Files

---
+++ tests/Day 9/Classroom Activity/typeAssertions.js
@@ -0,0 +1,7 @@
+"use strict";
+let response = "Success";
+let myResponse = response;
+console.log(myResponse.length);
+console.log(myResponse.toUpperCase());
+console.log(myResponse.concat("fully"));
+console.log(myResponse.lastIndexOf("s"));
---
+++ tests/Day 9/Classroom Activity/typeAssertions.ts
@@ -0,0 +1,6 @@
+let response : any = "Success"
+let myResponse = <string> response
+console.log(myResponse.length)
+console.log(myResponse.toUpperCase())
+console.log(myResponse.concat("fully"))
+console.log(myResponse.lastIndexOf("s"))
